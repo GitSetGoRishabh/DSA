fe680a648bf2bec68f076b4240f11aee5a278592
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-09-05 10:09:30 +0530

    pallindrome

--- Basic-Maths/palindrome.cpp
+++ Basic-Maths/palindrome.cpp
@@ -6,8 +6,19 @@ int main(){
     int num;
     cout<<"Enter the number:";
     cin>>num;
+    int temp = num;
     int revNum=0;
+    int lastDigit;
     while(num>0){
-
+        lastDigit=num%10;
+        revNum=revNum*10+lastDigit;
+        num=num/10;
     }
+    if(revNum==temp){
+        cout<<"True";
+    }
+    else{
+        cout<<"False";
+    }
+    return 0;
 }
