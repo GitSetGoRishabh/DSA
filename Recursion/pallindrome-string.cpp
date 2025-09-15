60c88409a8f5b6818236660ded4fd1cfea84eed6
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-09-15 16:11:52 +0530

    to check if string is a pallindrome using recursion

---
+++ Recursion/pallindrome-string.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int checkPallindrome(string s,int i){
+    if(i>=s.size()/2) return 1;
+    if(s[i]!=s[s.size()-i-1]){
+        return -1;
+    }
+    return checkPallindrome(s,i+1);
+}
+int main(){
+    string s="nitin";
+    cout<<checkPallindrome(s,0);
+}
