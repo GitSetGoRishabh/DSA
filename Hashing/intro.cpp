ce809149ffcbce0534194d02eda7755fc71597a4
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-09-17 22:26:15 +0530

    hashing intro

---
+++ Hashing/intro.cpp
@@ -0,0 +1,21 @@
+// hashing is used for pre storing and fetching.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    //precomputing
+    int hash[13];
+    for(int i=0;i<n;i++){
+        
+    }
+
+    return 0;
+}
