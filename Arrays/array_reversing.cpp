ce049a5d7bf8f40b27734d2a95c9df0781be72ea
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-12-17 19:29:18 +0530

    reversing array using two pointer

--- Arrays/array_reversing.cpp
+++ Arrays/array_reversing.cpp
@@ -9,4 +9,18 @@ int main(){
     for(int i=0;i<5;i++){
         cout<<arr[i];
     }
+
+    int arr2[5]={6,7,8,9,10};
+
+    int j=0;
+    int k=5;
+    while(j<k){
+        swap(arr2[j],arr2[k]);
+        j++;
+        k--;
+    }
+
+    for(int i=0;i<5;i++){
+        cout<<arr2[i];
+    }    
 }
