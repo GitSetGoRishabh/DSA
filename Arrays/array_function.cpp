c94213459d2025c52b41ba41ab05dc19e1ec566e
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-12-18 18:15:11 +0530

    passing array in a function

---
+++ Arrays/array_function.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fun(int a[],int n){
+    for(int i=0;i<n;i++){
+        cout<<a[i];
+    }
+}
+
+int main(){
+    int arr[5]={1,3,5,7,9};
+    fun(arr,5);
+}
