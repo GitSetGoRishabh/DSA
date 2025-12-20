c83275239d1312c2c65cd863cae3e242b0edf0d1
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-12-20 18:18:37 +0530

    bubble sort on an array of characters

--- Sorting/bubble-sort/bubble_character.cpp
+++ Sorting/bubble-sort/bubble_character.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int n=5;
-    int arr[5]={1,6,3,7,2};
+    char arr[5]={'a','i','u','o','e'};
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
             if (arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
@@ -12,5 +12,5 @@ int main(){
     for(int i=0;i<n;i++){
         cout<<arr[i];
     }
-    
+
 }
