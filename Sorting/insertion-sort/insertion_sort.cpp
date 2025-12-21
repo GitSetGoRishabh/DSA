701c8cd7b55b4688d8fe4e59936aea0d609e9c77
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-12-21 19:22:45 +0530

    insertion sort in ascending order

--- Sorting/insertion-sort/insertion_sort.cpp
+++ Sorting/insertion-sort/insertion_sort.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 
-void selectionSort(int arr[],int n){
-    for(int i=0;i<=n-2;i++){
-        int min=i;
-        for(int j=i;j<=n-1;j++){
-            if(arr[j]<arr[min]) min=j;
+void insertionSort(int arr[],int n){
+    for(int i=1;i<=n-1;i++){
+        for(int j=i;j>0;j--){
+            if(arr[j]<arr[j-1]) swap(arr[j],arr[j-1]);
+            else break;
         }
-        swap(arr[min],arr[i]);
+        
     }
 }
 
@@ -19,7 +19,7 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    selectionSort(arr,n);
+    insertionSort(arr,n);
     for(int i=0;i<n;i++){
         cout<<arr[i];
     }
