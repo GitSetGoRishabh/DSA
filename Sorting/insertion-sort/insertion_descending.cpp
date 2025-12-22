756290fb847170ee2c2facc20ebeb458cda10f97
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-12-22 15:11:42 +0530

    insertion sort in descending order

---
+++ Sorting/insertion-sort/insertion_descending.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n=5;
+    int arr[5]={1,6,3,5,4};
+
+    for(int i=0;i<n;i++){
+        for(int j=i;j>0;j--){
+            if(arr[j]>arr[j-1]) swap(arr[j],arr[j-1]);
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+}
