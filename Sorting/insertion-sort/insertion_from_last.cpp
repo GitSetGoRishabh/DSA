f5b66cbe67007668e9d62ae830ceb0bee1e5e644
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-12-22 21:44:35 +0530

    insertion sort from last

--- Sorting/insertion-sort/insertion_from_last.cpp
+++ Sorting/insertion-sort/insertion_from_last.cpp
@@ -5,7 +5,7 @@ int main(){
     int n=5;
     int arr[5]={1,6,4,5,2};
 
-    for(int i=n-1;i>=0;i--){
+    for(int i=n-2;i>=0;i--){
         for(int j=i;j<n-1;j++){
             if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
             else break;
