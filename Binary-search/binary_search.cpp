acb25f6fa9e1b37a9a61b844416da09da7207e7b
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-12-23 09:52:28 +0530

    binary search implementation

---
+++ Binary-search/binary_search.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n=6;
+    int arr[6]={2,4,5,7,9,11};
+    int key=7;
+
+    int start=0;
+    int end=n-1;
+    int mid;
+
+    while(start<=end){
+        mid=(start+end)/2;
+        if(arr[mid]==key){
+            cout<<"Found at index "<<mid;
+            break;
+        }
+        else if(arr[mid]>key) end=mid-1;
+        else start=mid+1;
+    }
+
+}
