52a04eddb49c2e1c21f6107e1930dca1b5a81c07
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-12-27 15:05:18 +0530

    second max using more optimised approach

--- Arrays/second_max.cpp
+++ Arrays/second_max.cpp
@@ -2,16 +2,31 @@
 using namespace std;
 
 int main(){
-    int largest = INT_MIN;
+
+    // int largest = INT_MIN;
     int arr[6]={1,5,7,4,2,8};
-    for(int i=0;i<6;i++){
-        if(largest<arr[i]) largest=arr[i];
-    }
-    int secondLargest=INT_MIN;
-    for(int i=0;i<6;i++){
-        if(arr[i]!=largest) secondLargest=max(arr[i],secondLargest);
+    // for(int i=0;i<6;i++){
+    //     if(largest<arr[i]) largest=arr[i];
+    // }
+    // int secondLargest=INT_MIN;
+    // for(int i=0;i<6;i++){
+    //     if(arr[i]!=largest) secondLargest=max(arr[i],secondLargest);
         
+    // }
+
+
+    // more optimised
+
+    int maximum=arr[0];
+    int secMaximum=INT_MIN;
+
+    for(int i=1;i<6;i++){
+        if(arr[i]>maximum){
+            secMaximum=maximum;
+            maximum=arr[i];
+        }
     }
-    cout<<largest<<endl;
-    cout<<secondLargest;
+
+    cout<<maximum<<endl;
+    cout<<secMaximum<<endl;
 }
