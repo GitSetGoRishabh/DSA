310547b5008fa829048e4d2b68d09f19bf45121e
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2026-02-05 23:51:53 +0530

    pascals's triangle

---
+++ Arrays/pascal's-triangle.cpp
@@ -0,0 +1,12 @@
+/*
+intuition : we take the edges of the pascal triangle as 1 for all rows ,
+then from the 3rd row we start adding the nums that are 
+top-left top-right to that element to form new element 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n=6;
+    
+}
