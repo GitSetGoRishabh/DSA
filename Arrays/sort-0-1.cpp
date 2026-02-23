e4d86fcfd97034a419f3119ab369052f2e9a7f64
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2026-02-23 12:21:23 +0530

    sort array with 0 and 1

---
+++ Arrays/sort-0-1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> arr={1,0,1,1,0,1,0,1,1,1};
+
+    int i=0;
+    int j=arr.size()-1;
+
+    while(i<j){
+        if(arr[i]==0) i++;
+        else if(arr[j]==1) j--;
+        else if(arr[i]==1 && arr[j]==0) {
+            swap(arr[i],arr[j]);
+            i++;
+            j--;
+        }
+    }
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i];
+    }
+    cout<<endl;
+}
