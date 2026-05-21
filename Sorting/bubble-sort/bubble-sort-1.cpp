6c23d01d504238fce3d42cc1554313c118c79bde
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2026-05-21 10:49:47 +0530

    sorting revision

--- Sorting/bubble-sort/bubble-sort-1.cpp
+++ Sorting/bubble-sort/bubble-sort-1.cpp
@@ -5,9 +5,15 @@ int main(){
     int arr[6]={3,8,2,9,10,7};
     int n=6;
     for(int i=0;i<n;i++){
+        int swapped =0;
         for(int j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
+            
+            if(arr[j]>arr[j+1]){
+                swapped=1;
+                swap(arr[j],arr[j+1]);
+            } 
         }
+        if(swapped==0) break;
     }
 
     for(int i=0;i<n;i++){
