5d50b7327b5d8e6b2111b13397ed5329b398c919
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2026-06-17 01:21:56 +0530

    basic bit manipulation operators

---
+++ Bit-manipulation/operators.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a=5;
+    int b=6;
+
+    cout<<(a&b); // and operator
+    cout<<(a|b); // or operator
+    cout<<(a^b); // xor operator
+    cout<<~(a); // not operator
+
+    cout<<(a>>2); //right shift
+    cout<<(a<<2); //left shift
+    
+
+}
