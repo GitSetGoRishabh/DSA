059c238fba90e44bb9dad7c82c9e8b3a4902028b
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2026-07-03 02:31:13 +0530

    array conversion to linked list

--- Linked-List/array-to-ll.cpp
+++ Linked-List/array-to-ll.cpp
@@ -9,10 +9,26 @@ struct node{
         data=data1;
         next=next1;
     }
+    public:
+    node(int data1){
+        data=data1;
+        next=nullptr;
+    }
 };
+
+node* convertArr2LL(vector<int> &arr){
+    node* head=new node(arr[0]);
+    node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        node* temp=new node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
 
 int main(){
     vector<int> arr={2,5,8,7};
-    node* y = new node(arr[0],nullptr);
-    cout<<y;
+    node* head =  convertArr2LL(arr);
+    cout<<head->data;
 }
