b1c4993a759ad09f31a5ec5c80918ca86f264fcc
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2026-07-03 02:50:21 +0530

    length of the linked list

---
+++ Linked-List/ll-length.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+    public:
+    int data;
+    node* next;
+    public:
+    node(int data1,node* next1){
+        data=data1;
+        next=next1;
+    }
+    public:
+    node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+
+node* convertArr2LL(vector<int> &arr){
+    node* head=new node(arr[0]);
+    node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        node* temp=new node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+
+int lengthOfLL(node* head){
+    int count=0;
+    node* temp=head;
+    while(temp){
+        count++;
+        temp=temp->next;
+    }
+    return count;
+}
+
+int main(){
+    vector<int> arr={2,5,8,7};
+    node* head =  convertArr2LL(arr);
+    
+    int len=lengthOfLL(head);
+    cout<<len<<endl;
+}
