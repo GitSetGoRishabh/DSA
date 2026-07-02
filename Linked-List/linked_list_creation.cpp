e447ac8c36f10c9be77bd6b57611000e49450bd9
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2026-07-03 00:58:31 +0530

    creating a node

--- Linked-List/linked_list_creation.cpp
+++ Linked-List/linked_list_creation.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-struct node{
+class node{
     public:
     int data;
     node* next;
@@ -11,8 +11,11 @@ struct node{
     }
 };
 
+
 int main(){
     vector<int> arr={2,5,8,7};
     node* y = new node(arr[0],nullptr);
     cout<<y;
+    //if used without * .. it will show error as it is just an object
+    //you can just access like y->data
 }
