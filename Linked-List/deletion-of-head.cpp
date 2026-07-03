48532b881f0135b28dca13520a9fcd0e438b7397
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2026-07-03 11:17:39 +0530

    deletion of head of a LL

---
+++ Linked-List/deletion-of-head.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+    public:
+    int data;
+    node* next;
+    public:
+    node(int data1,node* next1){
+        data=data1;
+        next=next1;
+    }
+    public:
+    node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+
+node* convertArr2LL(vector<int> &arr){
+    node* head=new node(arr[0]);
+    node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        node* temp=new node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+
+node* deleteHead(node* head){
+    if(head==NULL) return head;
+    head=head->next;
+    return head;
+}
+
+int main(){
+    vector<int> arr={2,5,8,7};
+    node* head =  convertArr2LL(arr);
+    head=deleteHead(head);
+    cout<<head;
+    
+}
