bf42f02cf9ec0c1e8b268649a66c6d32c9d4f190
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2026-08-04 20:27:14 +0530

    stack implementatin using arrays

---
+++ Stack-Queues/stack-implementation-array.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+class arrayStack{
+private:
+    int* stackArray;
+    int capacity;
+    int topIndex;
+public:
+    arrayStack(int size=1000){
+        capacity=size;
+        stackArray=new int[capacity];
+        topIndex=-1;
+    }
+    ~arrayStack(){
+        delete[] stackArray;
+    }
+    void push(int x){
+        if(topIndex>=capacity-1){
+            cout<<"Stack overflow"<<endl;
+            return;
+        }
+        topIndex++;
+        stackArray[topIndex]=x;
+    }
+    
+    bool isEmpty(){
+        return topIndex==-1;
+    }
+
+    int pop(){
+        if(isEmpty()){
+            cout<<"Stack is empty"<<endl;
+            return -1;
+        }
+        return stackArray[topIndex--];
+    }
+
+    int top(){
+        if(isEmpty()) {
+            cout<<"Stack is empty"<<endl;
+            return -1;
+        }
+        return stackArray[topIndex];
+    }
+    
+};
+int main() {
+    arrayStack stack;
+    vector<string> commands = {"ArrayStack", "push", "push", "top", "pop", "isEmpty"};
+    vector<vector<int>> inputs = {{}, {5}, {10}, {}, {}, {}};
+
+    for (size_t i = 0; i < commands.size(); ++i) {
+        if (commands[i] == "push") {
+            stack.push(inputs[i][0]);
+            cout << "null ";
+        } else if (commands[i] == "pop") {
+            cout << stack.pop() << " ";
+        } else if (commands[i] == "top") {
+            cout << stack.top() << " ";
+        } else if (commands[i] == "isEmpty") {
+            cout << (stack.isEmpty() ? "true" : "false") << " ";
+        } else if (commands[i] == "ArrayStack") {
+            cout << "null ";
+        }
+    }
+    return 0;
+}
