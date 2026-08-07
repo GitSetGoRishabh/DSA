d3502b9e04db51b083f065000fe79c3e002c5ba7
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2026-08-07 08:19:04 +0530

    printing 1 to n using recurision backtracking

--- Recursion/1-to-n-backtrackintg.cpp
+++ Recursion/1-to-n-backtrackintg.cpp
@@ -1,16 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void printOnetoN(int i,int n){
+void printOnetoN(int i){
     if(i<1) return;
 
-    printOnetoN(i-1,n);
+    printOnetoN(i-1);
     cout<<i<<endl;
 }
 
 int main(){
     int num;
     cin>>num;
-    printOnetoN(num,num);
+    printOnetoN(num);
     return 0;
 }
